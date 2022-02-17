fd1263f7c30bad57eb1b1ba713c6fb674cbaab98
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2022-02-17 15:53:01 +0300

    REMOVE ME: tweak debug info for runtime

--- kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/bitcode/CompileToBitcode.kt
+++ kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/bitcode/CompileToBitcode.kt
@@ -134,6 +134,7 @@ abstract class CompileToBitcode @Inject constructor(
         get() {
             val commonFlags = listOfNotNull(
                     "-gdwarf-2".takeIf { project.kotlinBuildProperties.isNativeRuntimeDebugInfoEnabled },
+                    "-gline-tables-only".takeIf { project.kotlinBuildProperties.isNativeRuntimeDebugInfoEnabled },
                     "-c", "-emit-llvm") + headersDirs.map { "-I$it" }
             val sanitizerFlags = when (sanitizer) {
                 null -> listOf()

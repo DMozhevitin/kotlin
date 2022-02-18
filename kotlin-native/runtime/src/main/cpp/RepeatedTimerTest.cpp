b86632c9c7f4dbf85f0fc7d37ae4cecb7bacd5f6
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2022-02-18 17:10:46 +0300

    Fix repeated tests

--- kotlin-native/runtime/src/main/cpp/RepeatedTimerTest.cpp
+++ kotlin-native/runtime/src/main/cpp/RepeatedTimerTest.cpp
@@ -18,9 +18,7 @@ TEST(RepeatedTimerTest, WillNotExecuteImmediately) {
     Clock::reset();
 
     std::atomic<int> counter = 0;
-    RepeatedTimer<Clock> timer(minutes(10), [&counter]() {
-        ++counter;
-    });
+    RepeatedTimer<Clock> timer(minutes(10), [&counter]() { ++counter; });
     // The function is not executed immediately.
     EXPECT_THAT(counter.load(), 0);
 }
@@ -29,15 +27,17 @@ TEST(RepeatedTimerTest, WillRun) {
     Clock::reset();
 
     std::atomic<int> counter = 0;
-    RepeatedTimer<Clock> timer(minutes(10), [&counter]() {
-        ++counter;
-    });
-    while (!Clock::pending()) {}
+    RepeatedTimer<Clock> timer(minutes(10), [&counter]() { ++counter; });
+    while (!Clock::pending()) {
+    }
     Clock::sleep_for(minutes(10));
-    while (counter < 1) {}
-    while (!Clock::pending()) {}
+    while (counter < 1) {
+    }
+    while (!Clock::pending()) {
+    }
     Clock::sleep_for(minutes(10));
-    while (counter < 2) {}
+    while (counter < 2) {
+    }
 }
 
 TEST(RepeatedTimerTest, WillStopInDestructor) {
@@ -54,9 +54,11 @@ TEST(RepeatedTimerTest, WillStopInDestructor) {
             ++counter;
         });
         // Wait until the counter increases once.
-        while (!Clock::pending()) {}
+        while (!Clock::pending()) {
+        }
         Clock::sleep_for(minutes(10));
-        while (counter < 1) {}
+        while (counter < 1) {
+        }
     }
     future.wait();
     // The thread has definitely finished.
@@ -71,11 +73,10 @@ TEST(RepeatedTimerTest, InfiniteInterval) {
 
     constexpr auto infinite = Clock::duration::max();
     std::atomic<int> counter = 0;
-    RepeatedTimer<Clock> timer(infinite, [&counter]() {
-        ++counter;
-    });
+    RepeatedTimer<Clock> timer(infinite, [&counter]() { ++counter; });
     // Wait until the clock starts waiting.
-    while (!Clock::pending()) {}
+    while (!Clock::pending()) {
+    }
     // Clock will wait the absolute maximum time.
     EXPECT_THAT(*Clock::pending(), Clock::time_point::max());
 }
@@ -86,38 +87,43 @@ TEST(RepeatedTimerTest, Restart) {
     auto begin = Clock::now();
 
     std::atomic<int> counter = 0;
-    RepeatedTimer<Clock> timer(minutes(10), [&counter]() {
-        ++counter;
-    });
+    RepeatedTimer<Clock> timer(minutes(10), [&counter]() { ++counter; });
     // Wait until the clock starts waiting.
-    while (!Clock::pending()) {}
+    while (!Clock::pending()) {
+    }
 
     // Now restart the timer to fire in 10 seconds instead.
     timer.restart(seconds(10));
-    while (!Clock::pending() || *Clock::pending() != begin + seconds(10)) {}
+    while (!Clock::pending() || *Clock::pending() != begin + seconds(10)) {
+    }
 
     // Now wait until task triggers once and is scheduled again with the same interval.
     Clock::sleep_until(begin + seconds(10));
-    while (!Clock::pending() || *Clock::pending() != begin + seconds(20)) {}
+    while (!Clock::pending() || *Clock::pending() != begin + seconds(20)) {
+    }
     EXPECT_THAT(counter.load(), 1);
 
     // Wait 5 seconds and restart the timer to fire in 1 minute time.
     Clock::sleep_for(seconds(5));
     timer.restart(minutes(1));
-    while (!Clock::pending() || *Clock::pending() != begin + seconds(15) + minutes(1)) {}
+    while (!Clock::pending() || *Clock::pending() != begin + seconds(15) + minutes(1)) {
+    }
 
     // And immediately restart the timer again to run in 5 minutes instead.
     timer.restart(minutes(5));
-    while (!Clock::pending() || *Clock::pending() != begin + seconds(15) + minutes(5)) {}
+    while (!Clock::pending() || *Clock::pending() != begin + seconds(15) + minutes(5)) {
+    }
 
     // And just restart the timer to fire after "infinite" interval.
     timer.restart(Clock::duration::max());
-    while (!Clock::pending() || *Clock::pending() != Clock::time_point::max()) {}
+    while (!Clock::pending() || *Clock::pending() != Clock::time_point::max()) {
+    }
 
     // Wait for an hour and restart the timer to fire every 10 minutes.
     Clock::sleep_for(hours(1));
     timer.restart(minutes(10));
-    while (!Clock::pending() || *Clock::pending() != begin + seconds(15) + hours(1) + minutes(10)) {}
+    while (!Clock::pending() || *Clock::pending() != begin + seconds(15) + hours(1) + minutes(10)) {
+    }
 
     // The counter only fired once.
     EXPECT_THAT(counter.load(), 1);
@@ -137,18 +143,23 @@ TEST(RepeatedTimerTest, RestartFromTask) {
     timerPtr = &timer;
 
     // Wait until the clock starts waiting.
-    while (!Clock::pending()) {}
+    while (!Clock::pending()) {
+    }
     EXPECT_THAT(*Clock::pending(), Clock::now() + minutes(10));
 
     // Make the task execute once and restart the task to fire every minute.
     Clock::sleep_for(minutes(10));
-    while (counter < 1) {}
-    while (!Clock::pending()) {}
+    while (counter < 1) {
+    }
+    while (!Clock::pending()) {
+    }
     EXPECT_THAT(*Clock::pending(), Clock::now() + minutes(1));
 
     // Now wait for that minute and make sure the next task is also scheduled 1 minute from now.
     Clock::sleep_for(minutes(1));
-    while (counter < 2) {}
-    while (!Clock::pending()) {}
+    while (counter < 2) {
+    }
+    while (!Clock::pending()) {
+    }
     EXPECT_THAT(*Clock::pending(), Clock::now() + minutes(1));
 }
